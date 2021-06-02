28f98686078cbe82f83bcf24a9912d8669884492
Aradhana Bhadran <aradhanab66@gmail.com>
2021-06-02 23:16:02 +1200

    First commit

---
+++ PlatinWaschee/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlatinWaschee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult aboutus()
+        {
+            return View();
+        }
+        public ActionResult contactus()
+        {
+            return View();
+        }
+        public ActionResult gallery()
+        {
+            return View();
+        }
+        public ActionResult offers()
+        {
+            return View();
+        }
+        public ActionResult packages()
+        {
+            return View();
+
+        }
+        public ActionResult services()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlatinWaschee/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlatinWaschee.MvcApplication" Language="C#" %>
